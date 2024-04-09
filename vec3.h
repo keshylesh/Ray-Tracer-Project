8bb7e55340bdb22b583a79cd2aabda1d77935939
keshylesh <keshavshylesh@gmail.com>
2024-04-09 03:11:53 -0400

    Added more vector functions to vec3.h

--- vec3.h
+++ vec3.h
@@ -17,13 +17,13 @@ class vec3 {
 		double y() const { return e[1]; }
 		double z() const { return e[2]; }
 
-		//Negate vector
+		// Negate vector
 		vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
 		
 		double operator[](int i) const { return e[i]; }
 		double& operator[](int i) { return e[i]; }
 
-		//Vector addition
+		// Vector addition
 		vec3& operator+=(const vec3 &v) {
 			e[0] += v.e[0];
 			e[1] += v.e[1];
@@ -31,10 +31,47 @@ class vec3 {
 			return *this;
 		}
 
-		//Scalar multiplication
+		// Scalar multiplication
 		vec3& operator*=(double t) {
 			e[0] *= t;
 			e[1] *= t;
 			e[2] *= t;
 		}
+		
+		vec3& operator/=(double t) {
+			return *this *= 1 / t;
+		}
+
+		double length() const {
+			return sqrt(length_squared());
+		}
+
+		double length_squared() const {
+			return (e[0] * e[0]) + (e[1] * e[1]) + (e[2] * e[2]);
+		}
+};
+
+// point3 just an alias for vec3, for geometric clarity
+using point3 = vec3 
+
+// Vector utiliy functions
+
+// Print vector
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+// Vector addition
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+// Vector subtraction
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
