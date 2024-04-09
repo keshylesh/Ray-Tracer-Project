775300d6859fcfbace0735aa2ab5ec9573293f52
keshylesh <keshavshylesh@gmail.com>
2024-04-09 03:19:11 -0400

    Added remaining dot product and cross product functions to vec3.h

--- vec3.h
+++ vec3.h
@@ -75,3 +75,35 @@ inline vec3 operator-(const vec3 &u, const vec3 &v) {
 inline vec3 operator*(const vec3 &u, const vec3 &v) {
 	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
+
+inline vec3 operator*(double t, const vec3 &v) {
+	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+	return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+	return (1 / t) * v;
+}
+
+// Dot product
+inline double dot(const vec3 &u, const vec3 &v) {
+	return (u.e[0] * v.e[0]) + (u.e[1] * v.e[1]) + (u.e[2] * v.e[2]);
+}
+
+// Cross product
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+	return vec3(
+		(u.e[1] * v.e[2]) - (u.e[2] * v.e[1]),
+		(u.e[2] * v.e[0]) - (u.e[0] * v.e[2]),
+		(u.e[0] * v.e[1]) - (u.e[1] * v.e[0])
+	);
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
+
+#endif
