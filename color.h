981875428ef34637093c45b78b19d4766132540f
keshylesh <keshavshylesh@gmail.com>
2024-11-10 02:28:11 -0500

    Added antialising common.h: added functionality to generate pseudo random values within a range interval.h: added clamp function to restrict a value between the max and min color.h: updated to use the clamp function for clamping the rgb pixel values camera.h: added code to allow color transformation based on the random samples, added a variable for samples_per_pixel main.cpp: added code to adjust the sample_per_pixel Outputs: build/antialiasing.exe: antialising with 100 samples per pixel, generated image_outputs/antialising.ppm build/antialiasing_1000_samples.exe: antialising test with 1000 samples per pixel, generated image_outputs/antialiasing_1000.ppm

--- color.h
+++ color.h
@@ -1,16 +1,24 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using color = vec3;
 
 void write_color(std::ostream &out, color pixel_color) {
-	// Translated value fo each component
-	
-	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-			<< static_cast<int>(255.999 * pixel_color.y()) << ' '
-			<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	// Translate [0, 1] range to [0, 255] range
+	static const interval intensity(0.000, 0.999);
+	int rbyte = int(256 * intensity.clamp(r));
+	int gbyte = int(256 * intensity.clamp(g));
+	int bbyte = int(256 * intensity.clamp(b));
+
+
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
 #endif
