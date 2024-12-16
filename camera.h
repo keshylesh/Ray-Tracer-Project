54e8d7ed4700b3be9f6da578ef5a45e6b209a2e1
keshylesh <keshavshylesh@gmail.com>
2024-12-16 04:17:41 -0500

    Diffuse materials and gamma correction
    Added code for diffuse materials and ray reflection, added code for gamma correction
    
    build/diff_mat_gray.exe: diffuse materials to produce gray rays
    
    build/diff_mat_ray_depth.exe: updated to limit the recursion depth of ray bounces
    
    build/shadow_acne_fix.exe: fixed shadow acne issues by changing the o to infinity interval to 0.001 to infinity
    
    build/lambertion_dist.exe: using a lambertion distribution to model ray reflections compared to randomly picking a direction
    
    build/gamma_corrected.exe: gamma corrects the image by using gamma 2

--- camera.h
+++ camera.h
@@ -9,6 +9,7 @@ class camera {
 	    double aspect_ratio = 16.0 / 9.0;
 	    int image_width = 400;
         int samples_per_pixel = 10; // Count of random samples for each pixel
+        int max_depth = 10; // Maximum number of ray bounces into scene (Limitting recursion depth)
 
         void render(const hittable& world) {
             initialize();
@@ -21,7 +22,7 @@ class camera {
 		            color pixel_color(0, 0, 0);
 		            for (int sample = 0; sample < samples_per_pixel; sample++) {
 		                ray r = get_ray(i, j);
-		                pixel_color += ray_color(r, world);
+		                pixel_color += ray_color(r, max_depth, world);
 		            }
 		            write_color(std::cout, pixel_samples_scale * pixel_color);
 		        }
@@ -85,10 +86,21 @@ class camera {
         }
 
         // ray_color method that was previously in main.cpp
-        color ray_color(const ray& r, const hittable& world) const {
+        color ray_color(const ray& r, int depth, const hittable& world) const {
+            // If depth exceeded, no more light gathered
+            if (depth <= 0) {
+                return color(0, 0, 0);
+            }
+
             hit_record rec;
-	        if (world.hit(r, interval(0, infinity), rec)) {
-		        return 0.5 * (rec.normal + color(1, 1, 1));
+            /*
+             * Interval from 0.001 instead of 0 to reduce floating point errors
+             * that cause the ray to possibly start below the surface. This causes
+             * extra collisions (shadow acne). By using 0.001 we ignore these rays
+             */
+	        if (world.hit(r, interval(0.001, infinity), rec)) {
+	            vec3 direction = rec.normal + random_unit_vector(); // Lambertion distribution
+		        return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
 	        }
 
     	    vec3 unit_direction = unit_vector(r.direction());
