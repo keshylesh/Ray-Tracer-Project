217602a0cde4738d3b73ada8e18b7160c6e81488
keshylesh <keshavshylesh@gmail.com>
2024-12-19 22:05:10 -0500

    Added Schlik's approximation for reflection, and a hollow sphere to observe multiple instances of refraction

--- material.h
+++ material.h
@@ -72,7 +72,7 @@ class dielectric : public material {
             bool cannot_refract = ri * sin_theta > 1.0;
             vec3 direction;
 
-            if (cannot_refract) {
+            if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
                 direction = reflect(unit_direction, rec.normal);
             }
             else {
@@ -85,6 +85,13 @@ class dielectric : public material {
 
     private:
         double refraction_index;
+
+        static double reflectance(double cosine, double refraction_index) {
+            // Uses Schlick's approximation for reflectance
+            auto r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0 * r0;
+            return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+        }
 };
 
 #endif
